046fb1cbbc07a53f2ca095a4692aa4a64cf64da2
Sadik Tekin <info@get-tek.uk>
2022-05-06 19:30:31 +0100

    fix cd for examples

--- examples/exemptions.tf
+++ examples/exemptions.tf
@@ -4,11 +4,11 @@ module "exemption_configure_asc_initiative" {
   name                 = "Onboard subscription to ASC Exemption"
   display_name         = "Exempted while testing"
   description          = "Excludes subscription from ASC onboarding during development"
-  scope                = data.azurerm_client_config.current.id
+  scope                = data.azurerm_subscription.current.id
   policy_assignment_id = module.org_mg_configure_asc_initiative.id
   policy_definition_reference_ids = [
-    "6d237bfef483fbb3308d",
-    "652d1284813c442a7e95"
+    "69908db02e3d4a578d03",
+    "1428ed118e4dcca5e747"
   ]
   exemption_category = "Waiver"
   expires_on         = "2023-05-25"
