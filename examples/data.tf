72e474c7a1835808d30dc7099d3db16319c9b817
Sadik Tekin <info@get-tek.uk>
2022-09-21 15:51:09 +0100

    2.6.3 (#48)
    * use policy_definition_reference_id
    * fix: #46 empty definition parameters

--- examples/data.tf
+++ examples/data.tf
@@ -1,24 +1,24 @@
-data "azurerm_client_config" "current" {}
+data azurerm_client_config current {}
 
-data "azurerm_subscription" "current" {}
+data azurerm_subscription current {}
 
 # Org Management Group
-data "azurerm_management_group" "org" {
+data azurerm_management_group org {
   name = "policy_dev"
 }
 
 # Child Management Group
-data "azurerm_management_group" "team_a" {
+data azurerm_management_group team_a {
   name = "team_a"
 }
 
 # Contributor role
-data "azurerm_role_definition" "contributor" {
+data azurerm_role_definition contributor {
   name = "Contributor"
 }
 
 # Virtual Machine Contributor role
-data "azurerm_role_definition" "vm_contributor" {
+data azurerm_role_definition vm_contributor {
   name = "Virtual Machine Contributor"
 }
 
--- modules/initiative/main.tf
+++ modules/initiative/main.tf
@@ -13,7 +13,7 @@ resource azurerm_policy_set_definition set {
     for_each = [for d in var.member_definitions : {
       id         = d.id
       ref_id     = replace(substr(title(replace(d.name, "/-|_|\\s/", " ")), 0, 64), "/\\s/", "")
-      parameters = jsondecode(d.parameters)
+      parameters = try(jsondecode(d.parameters), {})
       groups     = []
     }]
 
