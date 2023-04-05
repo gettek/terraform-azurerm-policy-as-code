f2c6164378b1ff675b2caaa6317e7193a15a27f1
gettek <info@get-tek.uk>
2023-04-05 16:28:23 +0100

    fix precommit

--- scripts/precommit.ps1
+++ scripts/precommit.ps1
@@ -1,53 +1,59 @@
 # PRECOMMIT TASKS
+# Requires 'terraform-docs' (https://terraform-docs.io/)
 
 [CmdletBinding()]
 Param(
     [switch] [Parameter(Mandatory = $False)] $tf,
     [switch] [Parameter(Mandatory = $False)] $library
 )
-try {
-    # Modules
-    Push-Location -Path $PSScriptRoot/../modules
-        (Get-ChildItem -Directory).BaseName | Foreach-Object {
+
+# Modules
+Push-Location -Path $PSScriptRoot/../modules
+(Get-ChildItem -Directory).BaseName | Foreach-Object {
+    try {
         Push-Location -Path $_
         Write-Host "📜 Generating '$_' Docs..." -ForegroundColor Magenta
         Get-Content TEMPLATE.md > README.md; "`n" >> README.md; terraform-docs md . >> README.md
         if ($tf) {
-            try {
-                terraform init -backend=false -upgrade
-                Write-Host "✅ Terraform fmt & validate '$_'..." -ForegroundColor Magenta
-                terraform fmt
-                terraform validate
-            }
-            finally {
-                Pop-Location
-            }
+            terraform init -backend=false -upgrade
+            Write-Host "✅ Terraform fmt & validate '$_'..." -ForegroundColor Magenta
+            terraform fmt
+            terraform validate
         }
     }
-    # Examples
-    Push-Location -Path $PSScriptRoot/../
-        (Get-ChildItem -Directory -Path examples*).BaseName | Foreach-Object {
-        Push-Location -Path $_
-        Write-Host "📜 Generating '$_' Docs..." -ForegroundColor Magenta
-        Get-Content TEMPLATE.md > README.md; "`n" >> README.md; terraform-docs md . >> README.md
-        if ($tf) {
-            try {
-                terraform init -backend=false -upgrade
-                Write-Host "✅ Terraform fmt & validate '$_'..." -ForegroundColor Magenta
-                terraform fmt
-                terraform validate
-            }
-            finally {
-                Pop-Location
-            }
-        }
+    catch {
+        Write-Host "🥵 Could not complete precommit tasks: $_" -ForegroundColor Red
     }
-    # Update custom definition library readme
-    if ($library) {
-        Write-Host "📜 Generating Custom Library Docs..." -ForegroundColor Magenta
-        & "$PSScriptRoot/markdown_generator.ps1"
+    finally {
+        Pop-Location
     }
-    # Return to original working directory
-    Pop-Location; Pop-Location
 }
-catch { Write-Host "🥵 Could not complete precommit tasks: $_" -ForegroundColor Red }
+# Examples
+Push-Location -Path $PSScriptRoot/../
+(Get-ChildItem -Directory -Path examples*).BaseName | Foreach-Object {
+    try {
+        Push-Location -Path $_
+        Write-Host "📜 Generating '$_' Docs..." -ForegroundColor Magenta
+        Get-Content TEMPLATE.md > README.md; "`n" >> README.md; terraform-docs md . >> README.md
+        if ($tf) {
+            terraform init -backend=false -upgrade
+            Write-Host "✅ Terraform fmt & validate '$_'..." -ForegroundColor Magenta
+            terraform fmt
+            terraform validate
+        }
+    }
+    catch {
+        Write-Host "🥵 Could not complete precommit tasks: $_" -ForegroundColor Red
+    }
+    finally {
+        Pop-Location
+    }
+}
+# Return to original working directory
+Pop-Location; Pop-Location
+
+# Update custom definition library readme
+if ($library) {
+    Write-Host "📜 Generating Custom Library Docs..." -ForegroundColor Magenta
+    & "$PSScriptRoot/markdown_generator.ps1"
+}
